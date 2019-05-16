1d65b4378326ca5471ecb7773178a90554cfe73a
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-05-16 12:21:27 +0530

    fix masks_path

--- dataset.jl
+++ dataset.jl
@@ -4,6 +4,7 @@ using Images, Luxor
 const base = "/home/shared/coco_images"
 const images_path = joinpath(base, "train2014")
 const annotations_path = joinpath(base, "annotations")
+const masks_path = joinpath(base, "masks")
 
 function coco_annotations(annotations_path = annotations_path)
   anns = filter(x -> occursin("json",x), readdir(annotations_path))
@@ -47,7 +48,7 @@ function get_class_ids()
 end
 
 function sample_coco(cid, images, classes;
-                    base_name = "COCO_train2014_000000"; masks = Dict())
+                    base_name = "COCO_train2014_000000", masks = Dict())
 
   img_class = sample(classes)
   img_id = sample(cid[img_class])
@@ -60,17 +61,17 @@ function sample_coco(cid, images, classes;
       Images.load(images_path * "/$base_name" * string(img_id) * ".jpg")
     end
   img_data = permutedims(channelview(img_data), (3,2,1))
-  mask, masks = make_masks(segmentation, size(img_data)[1:2], img["file_name"], masks)
+  mask = zeros(Float32, size(img_data)[1:2]...)
   
   # Don't resize
   # img_data = imresize(img_data, 299,299)
-  img_data, mask, img_class, rpn_bbox, masks
+  img_data, mask, img_class, rpn_bbox
 end
 
-function sample_coco(id::Int, images; base_name = "COCO_train2014_000000"; masks = Dict())
+function sample_coco(id::Int, images; base_name = "COCO_train2014_000000", masks = Dict())
   img = images[id]
   if haskey(img, "file_name")
-    img_data = Images.load(joinpath(base, img["file_name"]))
+    img_data = Images.load(joinpath(images_path, img["file_name"]))
   else
     try
       img_data = Images.load(images_path * "/$base_name" * string(id) * ".jpg")
@@ -83,9 +84,9 @@ function sample_coco(id::Int, images; base_name = "COCO_train2014_000000"; masks
   img_class = img["category_id"]
   segmentation = img["segmentation"]
   # mask = zeros(Float32, size(img_data)[1:2]...)
-  mask, masks = make_masks(segmentation, size(img_data)[1:2], img["file_name"], masks)
+  mask, masks = make_masks(segmentation, size(img_data)[1:2], img["file_name"], masks = masks)
   rpn_bbox = transpose(img["bbox"])
-  img_data, mask, img_class, rpn_bbox, masks
+  img_data, mask, img_class, rpn_bbox, segmentation, masks
 end
 
 function make_masks(segmentation::Array, image_size::Tuple, image_name; masks::Dict = Dict())
@@ -93,27 +94,30 @@ function make_masks(segmentation::Array, image_size::Tuple, image_name; masks::D
   polyY = segmentation[1][2:2:end]
 
   if !haskey(masks, image_name)
-    Drawing(image_size[1:2]..., joinpath(base, "masks/mask_" * image_name))
+    image_name = split(image_name, ".")[1] * ".png"
+    # @png begin
+    #   Drawing(image_size..., joinpath(base, "masks/mask_" * image_name))
+    #   ps = map(x -> Luxor.Point(x[2],x[1]), zip(polyY, polyX))
+    #   poly(ps, :fill)
+
+    # end
+
+    Drawing(image_size..., joinpath(masks_path, "mask_" * image_name))
     background("black")
     sethue("white")
     ps = map(x -> Luxor.Point(x[2],x[1]), zip(polyY, polyX))
     poly(ps, :fill)
     finish()
 
-    # @png begin
-    #   Drawing(image_size[1:2]..., joinpath(base, "masks/mask_" * image_name))
-    #   ps = map(x -> Luxor.Point(x[2],x[1]), zip(polyY, polyX))
-    #   poly(ps, :fill)
-    # end
-
-    masks[image_name] = joinpath(base, "masks/mask_"*image_name)
-    i = Images.load(joinpath(base, "masks/mask_"*image_name))
+    masks[image_name] = joinpath(masks_path, "mask_"*image_name)
+    i = Images.load(joinpath(masks_path, "mask_"*image_name))
     i, masks
   else
-    Images.load(joinpath(base, "masks/mask_"*image_name))
+    Images.load(joinpath(masks_path, "mask_"*image_name))
     i, masks
   end
 end
+
 
 
 
