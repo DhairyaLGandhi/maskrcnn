179fa6963f3250fc5d48ede0ea40bfe3ca22055a
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-05-22 12:34:16 +0530

    gpu runs

---
+++ dataset_gpu.jl
@@ -0,0 +1,214 @@
+using JSON, StatsBase
+using Images, Luxor
+using Images.ImageTransformations
+using Interpolations
+# using ImageView
+
+const base = "/home/shared/coco_images"
+const images_path = joinpath(base, "train2014")
+const annotations_path = joinpath(base, "annotations")
+const masks_path = joinpath(base, "masks")
+
+function coco_annotations(annotations_path = annotations_path)
+  anns = filter(x -> occursin("json",x), readdir(annotations_path))
+  n = Dict()
+
+  for i in anns
+    s = String(JSON.read(joinpath(annotations_path, i)))
+    s = JSON.parse(s)
+    n[i] = s
+  end
+
+  n
+end
+
+# COCO_train2014_000000581882.jpg
+# 480023
+
+# coco = coco_annotations()
+
+function get_class_ids()
+  class_ids = Integer[]
+  class_ids_dic = Dict()
+  images = Dict()
+  for v in coco["instances_train2014.json"]["annotations"]
+    push!(class_ids, v["category_id"])
+    if !haskey(class_ids_dic, v["category_id"])
+      class_ids_dic[v["category_id"]] = [v["image_id"]]
+    else
+      class_ids_dic[v["category_id"]] = push!(class_ids_dic[v["category_id"]], v["image_id"])
+    end
+
+    if !haskey(images, v["image_id"])
+      images[v["image_id"]] = Dict()
+      images[v["image_id"]]["vals"] = [v]
+    else
+      push!(images[v["image_id"]]["vals"], v)
+    end
+  end
+
+  for v in coco["instances_train2014.json"]["images"]
+    try
+      images[v["id"]]["file_name"] = v["file_name"]
+    catch ex
+    end
+  end
+  class_ids_dic, images, sort(unique(class_ids))
+end
+
+function sample_coco(cid, images, classes;
+                    base_name = "COCO_train2014_000000", masks = Dict())
+
+  img_class = sample(classes)
+  img_id = sample(cid[img_class])
+  img = images[img_id]
+  # segmentation = img["segmentation"]
+  # rpn_bbox = transpose(img["bbox"])
+  img_data = try
+      Images.load(joinpath(images_path, img["file_name"]))
+    catch KeyError
+      Images.load(images_path * "/$base_name" * string(img_id) * ".jpg")
+    end
+  img_data = permutedims(channelview(img_data), (3,2,1))
+  # mask = zeros(Float32, size(img_data)[1:2]...)
+  mask, bboxes, class_ids, masks = make_masks(img, size(img_data)[1:2], img["file_name"], masks = masks)
+  
+  # Don't resize
+  # img_data = imresize(img_data, 299,299)
+  img_data, mask, class_ids, bboxes, img_id, masks
+end
+
+function sample_coco(id::Int, images; base_name = "COCO_train2014_000000", masks = Dict())
+  img = images[id]
+  if haskey(img, "file_name")
+    img_data = Images.load(joinpath(images_path, img["file_name"]))
+  else
+    try
+      img_data = Images.load(images_path * "/$base_name" * string(id) * ".jpg")
+    catch ex
+      @show ex
+      throw(ex)
+    end
+  end
+  img_data = permutedims(channelview(img_data), (3,2,1))
+  # img_class = img["category_id"]
+  # segmentation = img["segmentation"]
+  # mask = zeros(Float32, size(img_data)[1:2]...)
+  # mask, masks = make_masks(segmentation, size(img_data)[1:2], img["file_name"], masks = masks)
+  mask, bboxes, class_ids, masks = make_masks(img, size(img_data)[1:2], img["file_name"], masks = masks)
+
+  # rpn_bbox = transpose(img["bbox"])
+  img_data, mask, class_ids, bboxes, id, masks
+end
+
+function make_masks(segmentation::Array, image_size::Tuple, image_name; masks::Dict = Dict())
+  polyX = segmentation[1][1:2:end]
+  polyY = segmentation[1][2:2:end]
+
+  if !haskey(masks, image_name)
+    image_name = split(image_name, ".")[1] * ".png"
+    # @png begin
+    #   Drawing(image_size..., joinpath(base, "masks/mask_" * image_name))
+    #   ps = map(x -> Luxor.Point(x[2],x[1]), zip(polyY, polyX))
+    #   poly(ps, :fill)
+
+    # end
+
+    Drawing(image_size..., joinpath(masks_path, "mask_" * image_name))
+    background("black")
+    sethue("white")
+    ps = map(x -> Luxor.Point(x[2],x[1]), zip(polyY, polyX))
+    poly(ps, :fill)
+    finish()
+
+    masks[image_name] = joinpath(masks_path, "mask_"*image_name)
+    i = Images.load(joinpath(masks_path, "mask_"*image_name))
+    i = permutedims(channelview(i), (3,2,1))
+    i = sum(i, dims = 3)
+    i, masks
+  else
+    i = Images.load(joinpath(masks_path, "mask_"*image_name))
+    i = colorview(Gray, i)
+    i = permutedims(channelview(i), (3,2,1))
+    i = sum(i, dims = 3)
+    i, masks
+  end
+end
+
+function make_masks(img::Dict, image_size::Tuple, image_name; masks = Dict())
+  img_masks = []
+  bboxes = []
+  class_ids = []
+  for i in img["vals"]
+    bb = transpose(i["bbox"])
+    mask, masks = make_masks(i["segmentation"], image_size, image_name; masks = masks)
+    push!(bboxes, bb)
+    push!(img_masks, mask)
+    if i["iscrowd"] == 0
+      push!(class_ids, i["category_id"])
+    else
+      push!(class_ids, -1)
+    end
+  end
+
+  cat(img_masks..., dims = 3), reduce(vcat, bboxes), class_ids, masks
+end
+
+
+function resize_mask(mask, scale, padding)
+  h, w = size(mask)[1:2]
+  z = zeros(round(Int, scale*w), round(Int, scale*h), size(mask, ndims(mask)))
+  s = BSpline(Cubic(Flat(OnCell())))
+  itp = interpolate(mask, (s, s, NoInterp()))
+  ImageTransformations.imresize!(z, itp)
+  z = round.(z)
+  z
+end
+
+function minimise_mask(bbox, mask, mini_shape)
+  s = (mini_shape..., size(mask, ndims(mask)))
+  mini_masks = zeros(s...)
+  # @show size(mask)
+  for (i, box) in zip(1:size(mask, ndims(mask)), eachrow(bbox))
+    # @show box
+    m = mask[:, :, i]
+    y1, x1, y2, x2 = box
+    m = m[x1:x2, y1:y2]
+    # @show size(m)
+    # @show "here"
+    m = imresize(m, mini_shape)
+    # @show size(m)
+    m = clamp.(m, 0., 1.)
+    mini_masks[:,:,i] = m
+  end
+  # mini_masks = round.(masks)
+  mini_masks
+end
+
+function load_image_gt(cid::Dict, images::Dict, classes; augment = true, use_mini_mask = true)
+  img_data, mask, class_ids, rpn_bbox, image_id, masks = sample_coco(cid, images, classes)
+  resized_img, window, scale, padding = resize_image(img_data, 1024, 1024, true)
+  mask = resize_mask(mask, scale, padding)
+  # return mask
+
+  # if augment && rand() > 0.5
+  #   resized_img = imshow(resized_img, flipx = true)
+  #   mask = imshow(mask, flipx = true)
+  # end
+
+  bbox = extract_bboxes(mask)
+
+  active_classes = zeros(length(classes))
+  active_classes .= 1
+
+  MINI_MASK_SHAPE = (56, 56)
+  if use_mini_mask
+    mask = minimise_mask(bbox, mask, MINI_MASK_SHAPE)
+    # mask = minimise_mask(clamp.(round.(Int, rpn_bbox), 1, size(img_data, 2)), mask, MINI_MASK_SHAPE)
+  end
+
+  image_meta = compose_image_meta(image_id, size(img_data), window, active_classes)
+
+  resized_img, image_meta, class_ids, bbox, mask
+end
+
---
+++ maskrcnn_gpu.jl
@@ -0,0 +1,677 @@
+using Flux, Flux.Tracker, Flux.Optimise
+using BSON: @load, @save
+using Images
+using Flux: @treelike
+import StatsBase.predict
+include("dataset.jl")
+
+# use Resnet from Metalhead
+struct ConvBlock
+    convlayer
+    norm
+    nonlinearity
+end
+
+@treelike ConvBlock
+
+function ConvBlock(kernel, chs; stride = (1, 1), pad = (0, 0))
+  # @show stride
+  stride = stride
+  ConvBlock(Conv(kernel, chs, stride = stride, pad = pad) |> gpu,
+
+  BatchNorm(chs[2]) |> gpu,
+
+  x -> relu.(x))
+end
+
+(c::ConvBlock)(x) = c.nonlinearity(c.norm(c.convlayer(x)))
+
+"""
+For Resnet (do it anyway), identity shortcut.
+"""
+struct IDBlock
+    path_1
+    shortcut
+end
+
+@treelike IDBlock
+
+function IDBlock(kernel, filters)
+    path_1 = Chain(ConvBlock((1,1), 3=>filters[1]),
+                ConvBlock(kernel, filters[1]=>filters[2]),
+                Conv((1,1), filters[2]=>filters[3]),
+                BatchNorm(filters[3])) |> gpu
+
+    IDBlock(path_1)
+end
+
+function (c::IDBlock)(x)
+    op1 = c.path_1(x)
+    relu.(op1 .+ x)
+end
+
+"""
+IDBlock but with a conv (and add) shortcut. Use in Resnet graph building.
+"""
+struct Conv_Block
+    path_1
+    shortcut
+end
+
+@treelike Conv_Block
+
+function Conv_Block(kernel, filters)
+    path_1 = Chain(ConvBlock((1,1), 3=>filters[1]),
+		ConvBlock(kernel, filters[1]=>filters[2]),
+		Conv((1,1), filters[2]=>filters[3]),
+		BatchNorm(filters[3])) |> gpu
+
+    shortcut = Chain(Conv((1,1), filters[2]=>filters[3]),
+                BatchNorm(filters[3])) |> gpu
+
+    Conv_Block(path_1, shortcut)
+end
+
+function (c::Conv_Block)(x)
+    op1 = c.path_1(x)
+    sc = c.shortcut(x)
+    relu.(op1 .+ sc)
+end
+
+struct ROIAlign
+  crop_height
+  crop_width
+  extrapolation_value
+  transform_fpcoor
+  # add params to hold
+end
+
+@treelike ROIAlign
+
+ROIAlign(crop_height, crop_width, extrapolation_value = 0.0f0, transform_fpcoor = true) = 
+  ROIAlign(crop_height, crop_width, extrapolation_value, transform_fpcoor)
+
+function (c::ROIAlign)(feature_map, boxes, box_ind)
+  x1 = boxes[:,1]
+  y1 = boxes[:,2]
+  x2 = boxes[:,3]
+  y2 = boxes[:,4]
+
+  img_height, img_width = Float.(size(feature_map)[1:2])
+
+  if c.transform_fpcoor
+    spacing_w = (x2 .- x1) ./ img_width
+    spacing_h = (y2 .- y1) ./ img_height
+
+    nx0 = (x1 .+ (spacing_w ./ 2.f0) .- 0.5f0) ./ (img_width .- 1.0f0)
+    ny0 = (y1 .+ (spacing_h ./ 2.f0) .- 0.5f0) ./ (img_height .- 1.0f0)
+    nw = spacing_w .* (c.crop_width .- 1.0f0) ./ (img_width .- 1.0f0)
+    nh = spacing_h .* (c.crop_height .- 1.0f0) ./ (img_height .- 1.0f0)
+    boxes = cat(ny0, nx0, ny0 .+ nh, nx0 .+ nw, dims = 2)
+  
+  else
+    x1 = x1 ./ (img_width .- 1.0f0)
+    x2 = x2 ./ (img_width .- 1.0f0)
+    y1 = y1 ./ (img_height .- 1.0f0)
+    y2 = y2 ./ (img_height .- 1.0f0)
+    boxes = cat(y1, x1, y2, x2, dims = 2)
+  end
+
+  crop_and_resize(feature_map, boxes, box_ind, c.crop_height, c.crop_width)
+end
+
+mutable struct FPN
+  C1
+  C2
+  C3
+  C4
+  C5
+  P6
+  P5_conv1
+  P5_conv2
+  P4_conv1
+  P4_conv2
+  P3_conv1
+  P3_conv2
+  P2_conv1
+  P2_conv2
+end
+
+@treelike FPN
+
+function FPN(out_channels, ps...)
+  cs = ps[1:5]
+  P6 = x -> maxpool(x, (1,1), stride = (2,2)) |> gpu
+  P5_conv1 = Conv((1,1), 2048=>out_channels) |> gpu
+  P5_conv2 = Conv((3,3), out_channels=>out_channels, pad = (1,1)) |> gpu
+
+  P4_conv1 = Conv((1,1), 1024=>out_channels) |> gpu
+  P4_conv2 = Conv((3,3), out_channels=>out_channels, pad = (1,1)) |> gpu
+
+  P3_conv1 = Conv((1,1), 512=>out_channels) |> gpu
+  P3_conv2 = Conv((3,3), out_channels=>out_channels, pad = (1,1)) |> gpu
+
+  P2_conv1 = Conv((1,1), 256=>out_channels) |> gpu
+  P2_conv2 = Conv((3,3), out_channels=>out_channels, pad = (1,1)) |> gpu
+
+  FPN(cs..., P6, P5_conv1,
+      P5_conv2,
+      P4_conv1,
+      P4_conv2,
+      P3_conv1,
+      P3_conv2,
+      P2_conv1,
+      P2_conv2)
+
+end
+
+function (c::FPN)(x)
+  c1_out = c.C1(x)
+  c2_out = c.C2(c1_out)
+  c3_out = c.C3(c2_out)
+  c4_out = c.C4(c3_out)
+  c5_out = c.C5(c4_out)
+
+  @show size(c5_out)
+  p5_out = c.P5_conv1(c5_out)
+
+  p4_out = c.P4_conv1(c4_out) + gpu(upsample(cpu(p5_out), (2,2,1,1)))
+  p3_out = c.P3_conv1(c3_out) + gpu(upsample(cpu(p4_out), (2,2,1,1)))
+  p2_out = c.P2_conv1(c2_out) + gpu(upsample(cpu(p3_out), (2,2,1,1)))
+
+  p5_out = c.P5_conv2(p5_out)
+  p4_out = c.P4_conv2(p4_out)
+  p3_out = c.P3_conv2(p3_out)
+  p2_out = c.P2_conv2(p2_out)
+
+  p6_out = c.P6(p5_out)
+
+  p2_out, p3_out, p4_out, p5_out, p6_out
+end
+
+mutable struct Mask
+  depth
+  pool_size
+  image_shape
+  num_classes
+  chain
+end
+
+@treelike Mask
+
+function Mask(depth, pool_size, image_shape, num_classes)
+  chain = Chain(ConvBlock((3,3), 256=>256, pad = (3,3)),
+                ConvBlock((3,3), 256=>256),
+                ConvBlock((3,3), 256=>256),
+                ConvTranspose((2,2), 256=>256, relu, stride = (2,2)),
+                Conv((1,1), 256=>num_classes, σ)) |> gpu
+
+  Mask(depth, pool_size, image_shape, num_classes, chain)
+end
+
+function (c::Mask)(x, rois)
+  op = pyramid_roi_align((rois, x...), c.pool_size, c.image_shape)
+  @show size(op)
+  # @show typeof(op)
+  op = gpu(op)
+  # @show typeof(op)
+  op = c.chain(op)
+  op
+end
+
+mutable struct Classifier
+  depth
+  pool_size
+  image_shape
+  num_classes
+  chain
+  linear_class
+  linear_bbox
+end
+
+@treelike Classifier
+
+function Classifier(depth, pool_size, image_shape, num_classes)
+  chain = Chain(ConvBlock((pool_size, pool_size), depth=>1024),
+              ConvBlock((1,1), 1024=>1024)) |> gpu
+  linear_class = Dense(1024, num_classes) |> gpu
+  linear_bbox = Dense(1024, 4*num_classes) |> gpu
+
+  Classifier(depth, 
+            pool_size,
+            image_shape,
+            num_classes,
+            chain,
+            linear_class,
+            linear_bbox)
+end
+
+function (c::Classifier)(x, rois)
+  # @show typeof(x)
+  x = pyramid_roi_align((rois, x...), c.pool_size, c.image_shape)
+  @show "passed pyramide"
+  @show size(x)
+  # @show typeof(collect(x))
+  x = gpu(x)
+  @show typeof(x)
+  x = c.chain(x)
+  @show size(x)
+  x = dropdims(x, dims = (1,2))
+  # x = transpose(x)
+  # @show size(x)
+  mrcnn_class_logits = c.linear_class(x)
+  mrcnn_probs = softmax(mrcnn_class_logits)
+  mrcnn_bbox = c.linear_bbox(x)
+  @warn size(mrcnn_bbox)
+  mrcnn_bbox = reshape(mrcnn_bbox, (:, 4, size(mrcnn_bbox, ndims(mrcnn_bbox))))
+
+  mrcnn_class_logits, mrcnn_probs, mrcnn_bbox
+end
+
+mutable struct RPN
+  # anchors_per_location
+  # anchor_stride
+  # depth
+  conv_shared
+  conv_class
+  conv_bbox
+end
+
+@treelike RPN
+
+function RPN(anchors_per_location::Int, anchor_stride, depth)
+  conv_shared = Conv((3,3), depth=>512, stride = anchor_stride, pad = (1,1)) |> gpu
+  conv_class = Conv((1,1), 512=>2*anchors_per_location) |> gpu
+  conv_bbox = Conv((1,1), 512=>4*anchors_per_location) |> gpu
+  RPN(
+    # anchors_per_location,
+    # anchor_stride,
+    # depth,
+    conv_shared,
+    conv_class,
+    conv_bbox)
+end
+
+function (c::RPN)(x)
+  x = relu.(c.conv_shared(x))
+
+  rpn_class_logits = c.conv_class(x)
+  rpn_class_logits = permutedims(rpn_class_logits, (3,4,2,1)) |> 
+                    x -> reshape(x, (2,:, size(rpn_class_logits, ndims(rpn_class_logits))))
+  @show maximum(rpn_class_logits)
+  @warn "rpn_class_logits"
+
+  ss = []
+  for i in 1:size(rpn_class_logits, ndims(rpn_class_logits))
+    y = selectdim(rpn_class_logits, ndims(rpn_class_logits), i)
+    push!(ss, softmax(y))
+  end
+  @show "no loop"
+  rpn_probs = cat(ss..., dims = 3)
+  @show maximum(rpn_probs)
+  @warn "rpn_probs"
+
+  rpn_bbox = c.conv_bbox(x)
+  rpn_bbox = permutedims(rpn_bbox, (3,4,2,1)) |> 
+                    x -> reshape(x, (4,:, size(rpn_bbox, ndims(rpn_bbox))))
+  @show maximum(rpn_bbox)
+  @warn "rpn_bbox"
+
+  rpn_class_logits, rpn_probs, rpn_bbox
+end
+
+mutable struct BottleNeck
+  chain
+  downsample
+end
+
+@treelike BottleNeck
+
+function BottleNeck(inplanes::Int, planes::Int; stride = (1,1), downsample=nothing)
+  # @show stride
+  chain = Chain(ConvBlock((1,1), inplanes=>planes,
+                                stride = stride, 
+                                pad = (0,0)),
+                ConvBlock((3,3), planes=>planes, pad = (1,1)),
+                Conv((1,1), planes=>4*planes),
+                BatchNorm(4*planes)) |> gpu
+  if downsample isa Nothing
+    downsample = identity
+  end
+  BottleNeck(chain, downsample)
+end
+
+function (c::BottleNeck)(x)
+  # @show "input size: $(size(x))"
+  residual = x
+  out = c.chain(x)
+  if !isa(c.downsample, Nothing)
+    residual = c.downsample(x)
+  end
+  # @show "before shortcut"
+  # @show size(out)
+  # @show size(residual)
+  # @show c.downsample
+  out = out .+ residual
+  # @show "after shortcut"
+  # @show "output size: $(size(out))"
+  relu.(out)
+end
+
+mutable struct ResNet
+  C1
+  C2
+  C3
+  C4
+  C5
+end
+
+@treelike ResNet
+
+function make_layer(block, planes, blocks, inplanes; stride = (1,1))
+  downsample = nothing  
+  if any([stride != (1,1), inplanes != planes*4])
+    downsample = Chain(Conv((1,1), inplanes => 4*planes, stride = stride),
+                      BatchNorm(4*planes)) |> gpu
+  end
+
+  layers = []
+  # return block(inplanes, planes; stride = stride, downsample = downsample)
+  push!(layers, block(inplanes, planes; stride = stride, downsample = downsample))
+  inplanes = planes * 4
+  for i = 1:(blocks-1)
+    push!(layers, block(inplanes, planes))
+  end
+
+  Chain(layers...) |> gpu
+end
+
+function ResNet(architecture::String; stage5::Bool = false)
+  inplanes = 64
+  if architecture == "resnet50"
+    layers = [3,4,6,3]
+  else
+    layers = [3,4,23,3]
+  end
+
+  C1 = Chain(ConvBlock((7,7), 3=>64, stride = (2,2), pad = (4 ,4)),
+            x -> maxpool(x, (3,3), stride = (2,2))) |> gpu
+
+  C2 = make_layer(BottleNeck, 64, layers[1], inplanes)
+  C3 = make_layer(BottleNeck, 128, layers[2], 256, stride = (2,2))
+  C4 = make_layer(BottleNeck, 256, layers[3], 512, stride = (2,2))
+  if stage5
+    C5 = make_layer(BottleNeck, 512, layers[4], 1024, stride = (2,2))
+  else
+    C5 = identity
+  end
+
+  ResNet(C1, C2, C3, C4, C5)
+end
+
+function (c::ResNet)(x)
+  x = c.C1(x)
+  x = c.C2(x)
+  x = c.C3(x)
+  x = c.C4(x)
+  x = c.C5(x)
+  x
+end
+
+mutable struct MaskRCNN
+  fpn
+  anchors
+  rpn
+  classifier
+  mask
+end
+
+@treelike MaskRCNN
+
+function build(config = nothing)
+  resnet = ResNet("resnet101", stage5=true)
+  fpn = FPN(256,
+            resnet.C1,
+            resnet.C2,
+            resnet.C3,
+            resnet.C4,
+            resnet.C5)
+
+  RPN_ANCHOR_SCALES = [32, 64, 128, 256, 512]
+  RPN_ANCHOR_RATIOS = [0.5, 1, 2]
+  BACKBONE_SHAPES = [256 256;
+                     128 128;
+                      64  64;
+                      32  32;
+                      16  16;]
+  BACKBONE_STRIDES = [4, 8, 16, 32, 64]
+  RPN_ANCHOR_STRIDE = 1
+  anchors = generate_pyramid_anchors(RPN_ANCHOR_SCALES,
+                                    RPN_ANCHOR_RATIOS,
+                                    BACKBONE_SHAPES,
+                                    BACKBONE_STRIDES,
+                                    RPN_ANCHOR_STRIDE)
+
+  rpn = RPN(length(RPN_ANCHOR_RATIOS), (RPN_ANCHOR_STRIDE,RPN_ANCHOR_STRIDE), 256)
+  POOL_SIZE = 7
+  IMAGE_SHAPE = [1024, 1024, 3]
+  NUM_CLASSES = 81
+  classifier = Classifier(256, POOL_SIZE, IMAGE_SHAPE, NUM_CLASSES)
+
+  mask = Mask(256, 2*POOL_SIZE, IMAGE_SHAPE, NUM_CLASSES)
+
+  MaskRCNN(
+    fpn,
+    anchors,
+    rpn,
+    classifier,
+    mask)
+
+end
+
+
+function detect(c::MaskRCNN, images)
+  molded_images, image_metas, windows = mold_inputs(images)
+  detections, mrcnn_mask = predict(molded_images, image_metas)
+end
+
+function predict(c::MaskRCNN, molded_images, image_metas,
+                gt_class_ids = nothing, gt_boxes = nothing,
+                gt_masks = nothing, mode = "inference")
+
+  p2_out, p3_out, p4_out, p5_out, p6_out = c.fpn(molded_images)
+  rpn_feature_maps = [p2_out, p3_out, p4_out, p5_out, p6_out]
+  mrcnn_feature_maps = [p2_out, p3_out, p4_out, p5_out]
+  @show "got fpn"
+
+  # for r in rpn_feature_maps
+  #   @show mean(r)
+  # end
+
+  # layer_outputs = []
+  # for p in rpn_feature_maps
+  #     push!(layer_outputs, c.rpn(p))
+  # end
+
+  c.rpn = cpu(c.rpn)
+  rpn_feature_maps = cpu.(rpn_feature_maps)
+  layer_outputs = map(c.rpn, rpn_feature_maps)
+  @show "rpn out"
+  # for lo in layer_outputs
+  #   for m in lo
+  #     @show size(m)
+  #     @show maximum(m)
+  #   end
+  # end
+
+  ops = zip(layer_outputs...)
+  ops2 = []
+  # for o in ops
+  #   push!(ops2, reduce(hcat, o))
+  # end
+  ops2 = map(x -> reduce(hcat, x), ops)
+  @show "reduction done"
+  # ops2 = gpu.(ops2)
+  rpn_class_logits, rpn_class, rpn_bbox = ops2
+  # @show maximum(rpn_class_logits)
+  # @show mean(rpn_class_logits)
+  # @show maximum(rpn_bbox)
+  # @show mean(rpn_bbox)
+  # @show maximum(rpn_class)
+  # @show mean(rpn_class)
+
+  POST_NMS_ROIS_TRAINING = 2000
+  POST_NMS_ROIS_INFERENCE = 1000
+  RPN_NMS_THRESHOLD = 0.7f0
+
+  if mode == "training"
+    proposal_count = POST_NMS_ROIS_TRAINING
+  else
+    proposal_count = POST_NMS_ROIS_INFERENCE
+  end
+
+  rpn_rois_arr = []
+  # global grpn_class = rpn_class
+  # global grpn_bbox = rpn_bbox
+  for i in 1:size(rpn_class, ndims(rpn_class))
+    rpn_class_slice = selectdim(rpn_class, ndims(rpn_class), i)    
+    rpn_bbox_slice = selectdim(rpn_bbox, ndims(rpn_bbox), i)    
+    # @show size(rpn_class_slice'), size(rpn_bbox_slice')
+    # @show maximum(rpn_bbox_slice)
+    # @show maximum(rpn_class_slice)
+    @info "At proposal # $i"
+    rpn_rois = proposal_layer([rpn_class_slice', rpn_bbox_slice'],
+                proposal_count,
+                RPN_NMS_THRESHOLD,
+                cpu(c.anchors))
+    @show size(rpn_rois)
+    push!(rpn_rois_arr, rpn_rois)
+  end
+  @show size(rpn_rois_arr[1])
+  # @show size(rpn_class)[end]
+  # rpn_rois = cat(rpn_rois_arr..., dims = 3)
+
+  # Make rpn_rois separate for every image. Currently, all images share
+  # all the ROIs.
+  rpn_rois = reduce(vcat, rpn_rois_arr)  # remove
+  @show typeof(rpn_rois)
+  rpn_rois = gpu(rpn_rois)
+  # @show rpn_rois
+  # error()
+  # return mrcnn_feature_maps, rpn_rois_arr
+  if mode == "inference"
+    for s in mrcnn_feature_maps
+      @show size(s)
+    end
+    # global grois = rpn_rois
+    mrcnn_class_logits, mrcnn_class, mrcnn_bbox = c.classifier(mrcnn_feature_maps, rpn_rois)
+
+    @show size(mrcnn_class_logits)
+    @show size(rpn_rois)
+
+    detections = detection_layer(rpn_rois, mrcnn_class, mrcnn_bbox, image_metas)
+    @show size(detections)
+
+    IMAGE_SHAPE = (1024, 1024, 3)
+    h, w = IMAGE_SHAPE[1:2]
+    scale = [h w h w]
+    detection_boxes = detections[:, 1:4] ./ scale
+
+    mrcnn_mask = c.mask(mrcnn_feature_maps, detection_boxes)
+    @show size(mrcnn_mask)
+
+    return [detections, mrcnn_mask]
+  elseif mode == "training"
+    # gt_class_ids = input[3]
+    # gt_boxes = input[4]
+    # gt_masks = input[5]
+
+    IMAGE_SHAPE = (1024, 1024, 3)
+    h, w = IMAGE_SHAPE[1:2]
+    scale = [h w h w] |> gpu
+
+    gt_boxes = gt_boxes ./ scale
+
+    rois, target_class_ids, target_deltas, target_mask =
+                detection_target_layer(rpn_rois, gt_class_ids,
+                                      gt_boxes, gt_masks)
+
+    mrcnn_class_logits, mrcnn_class, mrcnn_bbox = c.classifier(mrcnn_feature_maps, rois)
+
+    mrcnn_mask = c.mask(mrcnn_feature_maps, rois)
+    @warn "am i actually done?"
+
+    return (rpn_class_logits, rpn_bbox, target_class_ids, mrcnn_class_logits,
+            target_deltas, mrcnn_bbox, target_mask, mrcnn_mask)
+    # use o/p from here, add targets from datatset.jl
+    # compute loss here and push backprop
+  else
+    error("Mode $mode not implemented")
+  end
+  # mrcnn_feature_maps, rpn_rois_arr
+
+end
+
+function train_maskrcnn(c::MaskRCNN, dataset = "coco"; epochs = 100, images_per_batch = 2)
+  if dataset != "coco"
+    return "Define $dataset API, or use the COCO API"
+  end
+
+  cid, images, classes = get_class_ids()
+  c = build()
+  masks = Dict()
+
+  # ps_mask = params(c.mask.chain)
+  # ps_classifier = params(c.classifier)
+  # ps_fpn = params(c.fpn)
+  # ps_rpn = params(c.rpn)
+  # ps = Params([ps_fpn, ps_rpn, ps_classifier, ps_mask])
+  ps = params(c)
+  opt = Descent()
+
+  total_images = length(images)
+  for epoch in 1:epochs
+
+    # img_data, mask, img_class, rpn_bbox, masks = sample_coco(cid, images, classes, masks = masks)
+    image, image_metas, gt_class_ids, gt_boxes, gt_masks = load_image_gt(cid, images, classes)
+    # molded_image, image_metas, windows = mold_inputs((img_data,))
+    rpn_match, rpn_bbox = build_rpn_targets(size(image)[1:2], c.anchors, gt_class_ids, gt_boxes)
+
+    rpn_class_logits, rpn_pred_bbox, target_class_ids,
+    mrcnn_class_logits, target_deltas, mrcnn_bbox,
+    target_mask, mrcnn_mask = predict(c,
+                              image,
+                              image_metas,
+                              gt_class_ids,
+                              gt_boxes,
+                              gt_masks,
+                              "training")
+
+    # Oh God Why
+    # l1, l2, l3 = compute_losses(rpn_class_logits, rpn_bbox, target_class_ids,
+    #                             mrcnn_class_logits, target_deltas, mrcnn_bbox,
+    #                             target_mask, mrcnn_mask)
+    # rpn_class_loss = compute_rpn_class_loss(rpn_match, rpn_class_logits)
+    # rpn_bbox_loss = compute_rpn_bbox_loss(rpn_bbox, rpn_match, rpn_pred_bbox)
+    # mrcnn_class_loss = compute_mrcnn_class_loss(target_class_ids, mrcnn_class_logits)
+    # mrcnn_bbox_loss = compute_mrcnn_bbox_loss(target_deltas, target_class_ids, mrcnn_bbox)
+    # mrcnn_mask_loss = compute_mrcnn_mask_loss(target_mask, target_class_ids, mrcnn_mask)
+
+    # loss = rpn_class_loss + rpn_bbox_loss + mrcnn_class_loss + mrcnn_bbox_loss + mrcnn_mask_loss
+
+    gs = Tracker.gradient(ps) do
+      rpn_class_loss = compute_rpn_class_loss(rpn_match, rpn_class_logits)
+      rpn_bbox_loss = compute_rpn_bbox_loss(rpn_bbox, rpn_match, rpn_pred_bbox)
+      mrcnn_class_loss = compute_mrcnn_class_loss(target_class_ids, mrcnn_class_logits)
+      mrcnn_bbox_loss = compute_mrcnn_bbox_loss(target_deltas, target_class_ids, mrcnn_bbox)
+      mrcnn_mask_loss = compute_mrcnn_mask_loss(target_mask, target_class_ids, mrcnn_mask)
+
+      loss = rpn_class_loss + rpn_bbox_loss + mrcnn_class_loss + mrcnn_bbox_loss + mrcnn_mask_loss
+    end
+
+    update!(opt, ps, gs)
+  end
+end
+
+include("utils.jl")
+
