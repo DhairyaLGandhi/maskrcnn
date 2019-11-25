c3da6dcba0690b736b2932e4318d764126e77b85
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-11-25 12:30:13 +0530

    check payload

--- .fluxbot/trial.jl
+++ .fluxbot/trial.jl
@@ -7,7 +7,10 @@ const myauth = GitHub.authenticate(ENV["FLUXBOT_GITHUB_TOKEN"])
 
 function trial()
   @info ENV["GITHUB_EVENT_PATH"]
-
+  f = JSON.parsefile(ENV["GITHUB_EVENT_PATH"])
+  @show keys(f)
+  event = GitHub.event_from_payload!("issue_comment", f)
+  @show event
 end
 
 trial()
