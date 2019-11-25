e553dacabc0d022062efa492071b917bb940e0e9
Dhairya Gandhi <dhairya@juliacopmuting.com>
2019-11-25 11:59:37 +0530

    add gh actions + test script

---
+++ .fluxbot/trial.jl
@@ -0,0 +1,12 @@
+using GitHub
+using GitHub.HTTP
+using GitHub.JSON
+using Sockets
+
+const myauth = GitHub.authenticate(ENV["FLUXBOT_GITHUB_TOKEN"])
+
+function trial()
+  ENV["GITHUB_EVENT_PATH"]
+end
+
+trial()
